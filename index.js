fd7f741bc0bb16b6faa663d3d41a9d894f11ceca
Rob Schwartz <res7cd@virginia.edu>
2025-01-16 07:20:39 -0700

    feat: added trust anchor endpoint

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// The array of numbers
-const numbers = [1, 2, 13];
+const TRUST_ANCHOR_NAME = "issuer-registry";
 
-// Dynamically create endpoints for each number
-numbers.forEach((num) => {
-  app.get(`/test${num}`, (req, res) => {
-    res.send('a'); // Respond with 'a'
-  });
+// SSL/TLS certificates
+const options = {
+  key: fs.readFileSync('server.key'), // Replace with the path to your private key
+  cert: fs.readFileSync('server.crt'), // Replace with the path to your certificate
+};
+
+
+// // The array of numbers
+// const numbers = [1, 2, 13];
+
+// // Dynamically create endpoints for each number
+// numbers.forEach((num) => {
+//   app.get(`/test${num}`, (req, res) => {
+//     res.send('a'); // Respond with 'a'
+//   });
+// });
+
+
+// Trust anchor well-known endpoint
+app.get(`/${TRUST_ANCHOR_NAME}/.well-known/openid-federation`, (req, res) => {
+  res.send(); // Respond with 'a'
 });
 
 
+
+
+
+// Health check endpoint for pre-testing purposes
 app.get(`/health`, (req, res) => {
   res.send(); // Respond with 'a'
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}.`);
+// Start the server with HTTPS
+https.createServer(options, app).listen(port, () => {
+  console.log(`HTTPS Server listening on port ${port}.`);
 });
