9db015f21c29800928e1848cb3264c98f5ec1fc0
Rob Schwartz <res7cd@virginia.edu>
2025-02-01 12:14:51 -0700

    feat: added new tests for all basic endpoints

--- README.md
+++ README.md
@@ -11,8 +11,9 @@ sqlite3, express
 ## Key commands:
 
 Load schema.sql into DB: `npm run builddb`
+Load test data into DB: ``
 Generate server keys (HTTPS): `npm run keygen`
-Launch node: `node index.js`
+Launch webservice: `npm run webservice`
 Test endpoint: `curl -X GET http://localhost:3000/test1`
 
 ## How to push the latest SQLite database to AWS:
--- index.js
+++ index.js
@@ -7,12 +7,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const TRUST_ANCHOR_NAME = "issuer-registry";
+const ISSUERS_SUBFOLDER_NAME = "issuers";
+const THIS_URL = "https://sandbox123123.example.com"; // For determining internal path for fetch statement
 
 // SSL/TLS certificates
 const options = {
   key: fs.readFileSync('server.key'), // Replace with the path to your private key
   cert: fs.readFileSync('server.crt'), // Replace with the path to your certificate
 };
+
+// Startup checks:
+// Get most recent JWKs from external sources
+// Ensure that all private keys and public keys actually match
 
 
 
