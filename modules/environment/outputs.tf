8bdb3e7829b00142252bb0c7639689d060b31b2c
James Chartrand <jc.chartrand@gmail.com>
2025-06-20 11:28:49 -0400

    remove dynamo outputs from test and prod terraform modules

--- modules/environment/outputs.tf
+++ modules/environment/outputs.tf
@@ -3,16 +3,6 @@ output "lambda_function_name" {
   value = aws_lambda_function.issuer_registry.function_name
 }
 
-output "dynamodb_table_name" {
-  description = "Name of the DynamoDB table"
-  value = aws_dynamodb_table.issuers.name
-}
-
-output "dynamodb_table_arn" {
-  description = "ARN of the DynamoDB table"
-  value = aws_dynamodb_table.issuers.arn
-}
-
 output "cloudfront_domain_name" {
   description = "Domain name of the CloudFront distribution"
   value = aws_cloudfront_distribution.api_distribution.domain_name
